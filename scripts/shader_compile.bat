79a741564cd766300c4db845cb7b958a5ec7015b
Marin Ioan-Lică <hansi.marinioan@gmail.com>
2022-10-30 22:38:14 +0200

    Updated script to call for environment variable instead of absolute path

--- scripts/shader_compile.bat
+++ scripts/shader_compile.bat
@@ -1,2 +1,2 @@
-C:\VulkanSDK\1.3.216.0\Bin\glslc.exe ../Render-Engine/src/Shaders/shader.vert -o ../shaders/bin/vert.spv
-C:\VulkanSDK\1.3.216.0\Bin\glslc.exe ../Render-Engine/src/Shaders/shader.frag -o ../shaders/bin/frag.spv
+%VULKAN_SDK%\Bin\glslc.exe ../Render-Engine/src/Shaders/shader.vert -o ../shaders/bin/vert.spv
+%VULKAN_SDK%\Bin\glslc.exe ../Render-Engine/src/Shaders/shader.frag -o ../shaders/bin/frag.spv
